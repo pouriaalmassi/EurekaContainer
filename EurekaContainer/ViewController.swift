5339f9474c3611b166bdfbf2ba9c65576d275446
Pouria B. Almassi <pouriaba@gmail.com>
2017-10-26 12:11:20 +1100

    Initial Commit

---
+++ EurekaContainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EurekaContainer
+//
+//  Created by Pouria Almassi on 26/10/17.
+//  Copyright © 2017 Pouria Almassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
